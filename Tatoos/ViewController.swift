177fcffe523b8f853f56ac04d8dae477afdd7574
Hoan Tran <hoan@bluepego.com>
2016-11-11 13:45:51 -0800

    Added more core data logic

--- Tatoos/ViewController.swift
+++ Tatoos/ViewController.swift
@@ -13,15 +13,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        _ = DataManager.listStudents()
-        CoreDataStack.deleteAllData()
+//        CoreDataStack.deleteAllData()
+//        
+//        _ = DataManager.listStudents()
+//        CoreDataStack.deleteAllData()
+//        
+//        _ = DataManager.addStudent("A", "Team", 12)
+//        _ = DataManager.addStudent("B", "Group", 13)
+//        _ = DataManager.addStudent("C", "Rum", 14)
+//
+//        let students = DataManager.listStudents()
+//        DataManager.removeStudent(students[1])
+//        _ = DataManager.listStudents()
+//        
+//        
+//        _ = DataManager.listCourses()
+//        _ = DataManager.addCourse("Music 101")
+//        _ = DataManager.addCourse("Music 102")
+//        _ = DataManager.addCourse("Music 103")
+//        
+//        let courses = DataManager.listCourses()
+//        _ = DataManager.listCourses()
+//        
+//        let student = students[0] as Student
+//        let course = courses[0] as Course
+//        
+//        log.debug("\(student.firstName) \(student.lastName) has \(student.courses?.count) courses")
+//        log.debug("\(course.name) has \(course.students?.count) students")
+//        DataManager.enroll(students[0], courses: courses)
+//        log.debug("\(student.firstName) \(student.lastName) has \(student.courses?.count) courses")
+//        log.debug("\(course.name) has \(course.students?.count) students")
+//        
+//        DataManager.removeCourse(courses[0])
+//        _ = DataManager.listCourses()
+//        
+//        CoreDataStack.deleteAllData()
+//        _ = DataManager.listStudents()
+//        _ = DataManager.listCourses()
         
-        _ = DataManager.addStudent("A", "Team", 12)
-
-        _ = DataManager.listStudents()
-        CoreDataStack.deleteAllData()
-
-        _ = DataManager.listStudents()
     }
 
     override func didReceiveMemoryWarning() {
--- Tatoos/models/DataManager.swift
+++ Tatoos/models/DataManager.swift
@@ -10,7 +10,7 @@ import CoreData
 
 class DataManager {
     static let studentClassName:String = String(describing: Student.self)
-    static let courseClassName:String = String(describing: Student.self)
+    static let courseClassName:String = String(describing: Course.self)
     
     private init() {}
     
@@ -28,33 +28,11 @@ class DataManager {
         CoreDataStack.saveContext()
 
         return student
-        
-        
-//        let course:Course = NSEntityDescription.insertNewObject(forEntityName: courseClassName, into: CoreDataStack.getContext()) as! Course
-//        course.name = "Quantum Physics 402"
-//        
-//        student.addToCourses(course)
-//        
-//        CoreDataStack.saveContext()
-//        
-//        let fetchRequest:NSFetchRequest<Student> = Student.fetchRequest()
-//        
-//        do {
-//            let searchResults = try CoreDataStack.getContext().fetch(fetchRequest)
-//            
-//            print("student count: \(searchResults.count)")
-//            
-//            for result in searchResults as [Student] {
-//                print ("\(result.firstName!) \(result.lastName!) is \(result.age) years old")
-//            }
-//        } catch {
-//            print ("\(error)")
-//        }
-        
     }
     
     class func removeStudent(_ student: Student) {
         CoreDataStack.getContext().delete(student)
+        CoreDataStack.saveContext()
     }
     
     class func dropAllCourses(_ student: Student) {
@@ -62,12 +40,10 @@ class DataManager {
         for course in courses! {
             student.removeFromCourses((course as! Course))
         }
-//        for course: Course in courses! {
-//            student.removeFromCourses(course)
-//        }
     }
     
     class func enroll(_ student: Student, courses: [Course])->Void {
+        self.dropAllCourses(student)
         for course in courses {
             student.addToCourses(course)
         }
@@ -76,12 +52,11 @@ class DataManager {
     
 
     class func listStudents()->[Student] {
-        
         let fetchRequest:NSFetchRequest<Student> = Student.fetchRequest()
         
         do {
             let searchResults = try CoreDataStack.getContext().fetch(fetchRequest)
-            print("student count: \(searchResults.count)")
+            log.debug("student count: \(searchResults.count)")
 
             for result in searchResults as [Student] {
                 log.debug ("\(result.firstName!) \(result.lastName!) is \(result.age) years old")
@@ -100,18 +75,37 @@ class DataManager {
     
     // MARK: - Course
     
-//    class func addCourse(_ name: String)->Course{
-//        let uuid = NSUUID().uuidString
-//        let course:Course = NSEntityDescription.insertNewObject(forEntityName: courseClassName, into: CoreDataStack.getContext()) as! Course
-//        course.id = uuid
-//        CoreDataStack.saveContext()
-//        
-//        return course
-//    }
-//    
-//    class func getCourses()->[Course] {
-//    }
-//    
-//    class func getEnrollments(_ course)->[Student] {
-//    }
+    class func addCourse(_ name: String)->Course{
+        let uuid = NSUUID().uuidString
+        let course:Course = NSEntityDescription.insertNewObject(forEntityName: courseClassName, into: CoreDataStack.getContext()) as! Course
+        course.id = uuid
+        course.name = name
+        CoreDataStack.saveContext()
+        
+        return course
+    }
+    
+    class func removeCourse(_ course: Course) {
+        CoreDataStack.getContext().delete(course)
+        CoreDataStack.saveContext()
+    }
+
+    
+    class func listCourses()->[Course] {
+        let fetchRequest:NSFetchRequest<Course> = Course.fetchRequest()
+        
+        do {
+            let searchResults = try CoreDataStack.getContext().fetch(fetchRequest)
+            log.debug("course count: \(searchResults.count)")
+            
+            for result in searchResults as [Course] {
+                log.debug ("\(result.name)")
+            }
+            
+            return searchResults as [Course]
+        } catch {
+            log.severe("\(error)")
+            return [Course]()
+        }
+    }
 }
